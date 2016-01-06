d7b0607ad0af2eb612665274f1a294ba4c5dd39a
poxstone <poxstone@gmail.com>
2016-01-06 09:45:36 -0500

    fix google oauth2

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,19 +3,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '915763385159550', // your App ID
         'clientSecret'  : '673ce4b297aa123579978d492d6a3618', // your App Secret
-        'callbackURL'   : 'http://localhost:80/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'v9h8pwtCuROBTlZZU8eR3Jxd0',
         'consumerSecret'    : 'BhdTiABcS1EIjNlchKyvNJunJdopTkjyJykdLXvIEDhSlMc8aH',
-        'callbackURL'       : 'http://localhost:80/auth/twitter/callback'
+        'callbackURL'       : 'http://localhost/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '546344726865-uhepg7nl75dr0qpj8dodn7bvtdek2rpb.apps.googleusercontent.com',
         'clientSecret'  : 'WhcQ5np2xbfwAr2HszwSXWY3',
-        'callbackURL'   : 'http://localhost:80/auth/google/callback'
+        'callbackURL'   : 'http://localhost/auth/google/callback'
     }
 
 };
